6739129138f423e2374397049cb5fbcfc7c48f25
samanar <ahamdianrad.saman@gmail.com>
2019-04-06 09:57:54 +0430

    news model updated

--- database/models/news.js
+++ database/models/news.js
@@ -2,8 +2,33 @@ let Sequelize = require('sequelize');
 let sequelize = require('../config');
 
 const News = sequelize.define('news', {
-    id: {type: Sequelize.INTEGER(10), primaryKey: true, autoIncrement: true},
-    title: Sequelize.STRING,
+    id: {
+        type: Sequelize.INTEGER(10),
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: Sequelize.STRING,
+    },
+    title2: {
+        type: Sequelize.STRING,
+    },
+    lead: {
+        type: Sequelize.TEXT
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+
+    status: {
+        type: Sequelize.INTEGER,
+    },
+    MessageSubject: {
+        type: Sequelize.TEXT
+    },
+    messageBody: {
+        type: Sequelize.TEXT
+    },
     point: {
         type: Sequelize.INTEGER
     },
@@ -15,28 +40,6 @@ const News = sequelize.define('news', {
         type: Sequelize.INTEGER,
         defaultValue: 0
     },
-    source: {
-        type: Sequelize.STRING
-    },
-    lead: {
-        type: Sequelize.TEXT
-    },
-    content: {
-        type: Sequelize.TEXT
-    },
-    status: {
-        type: Sequelize.INTEGER,
-
-    },
-    message: {
-        type: Sequelize.TEXT
-    },
-    createdBy: {
-        type: Sequelize.STRING
-    },
-
-} );
+});
 
 module.exports = News;
-
-
